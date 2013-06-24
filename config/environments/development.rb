19aa805a0a8ff9ecd12baf6a64a50f7e7ebf226c
Craig <craigmcsavaney@gmail.com>
2013-06-24 10:56:00 -0600

    Installed Devise Gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Myapplication::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Added by Craig based on Devise configuration guidance:
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
