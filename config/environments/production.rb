445d7bc23222316ae15ae52f350c7f6f613aaebb
Craig <craigmcsavaney@gmail.com>
2013-07-13 08:24:36 -0600

    Heroku production email update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,10 +65,11 @@ Myapplication::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.default_url_options = { :host => 'morning-savannah-7661.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 587,
+    port:                 '587',
     domain:               'causebutton.com',
     user_name:            'craig@causebutton.com',
     password:             'USna21412',
