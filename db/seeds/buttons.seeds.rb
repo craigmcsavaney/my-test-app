9044207cc68ace6fe4c25ef9b113a02e7d23ba56
Craig <craigmcsavaney@gmail.com>
2013-09-29 15:58:24 -0600

    button seeds file changes

---
+++ db/seeds/buttons.seeds.rb
@@ -0,0 +1,18 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+after :button_types do
+
+puts 'Preparing to create seed Buttons'
+
+  	Button.find_or_create_by_name("No button (we'll add a button graphic ourselves)", description: "No button will be inserted. Merchant must provide its own button with the appropriate attributes.", button_type_id: ButtonType.find_by_name("CSS").id )
+	Button.find_or_create_by_name("Basic", description: "Basic CSS button", button_type_id: ButtonType.find_by_name("CSS").id )
+
+puts 'All seed Buttons found or created'
+
+end
