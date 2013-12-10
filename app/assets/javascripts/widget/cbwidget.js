0408b29a76310ebcea2193e176f326d5d2911e75
Craig <craigmcsavaney@gmail.com>
2013-12-10 14:39:05 -0700

    trying to get placeholder message visible on event dropdown

--- app/assets/javascripts/widget/cbwidget.js
+++ app/assets/javascripts/widget/cbwidget.js
@@ -527,10 +527,7 @@ function CBSale(amount,transaction_id) {
             var fgcause_select = $("#cbw-fgcause-select");
 
             // set the initial values of the event selector and the fgcause selector
-            cause_select.attr('value', ServeData.event_uid);
-            if (ServeData.event_uid && ServeData.event_uid != "") {
-                cause_select.select2("val", ServeData.event_uid);
-            }
+            cause_select.select2("val", ServeData.event_uid);
             fgcause_select.attr('value', ServeData.fg_uuid);
 
             // check the proper radio button based on the cause_type
