c4be79445c3b2d3837663beb3cb4e2a566e41b40
Craig <craigmcsavaney@gmail.com>
2013-12-31 10:21:48 -0700

    Clean-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,17 @@
 class ApplicationController < ActionController::Base
-	before_filter :set_page
+	before_filter :set_return_page
   	protect_from_forgery
 
   	rescue_from CanCan::AccessDenied do |exception|
     	redirect_to root_url, :alert => exception.message
   	end
 
- #  	def after_sign_in_path_for(resource)
-	#   user_path(current_user)
-	# end
 	def after_sign_in_path_for(resource)
 	    (session[:return_page].nil?) ? "/" : session[:return_page]
 	end
 
   	protected
-	def set_page
+	def set_return_page
 		unless request.referer.nil? || request.referer.include?('/users/sign')
   			session[:return_page] = request.referer
 		end
