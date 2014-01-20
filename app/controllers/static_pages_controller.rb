fe7f9db8f87d59852d6be9f2e5ad2882eda8f8fd
Craig <craigmcsavaney@gmail.com>
2014-01-20 13:39:20 -0700

    Mods to static pages controller home method to re-display contact message contents on failure to create

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,26 @@ class StaticPagesController < ApplicationController
 	layout false, only: :landing
 
   	def home
+      # create new message object for the contact_message form on the home page
+      @message = ContactMessage.new
+      # check to see if message params were passed in and if they were, use the values
+      # in the newly created message object.  These get passed in for all failed
+      # contact_message creations.  Successful creations pass back blank params.
+      if params[:first_name]
+        @message.first_name = params[:first_name]
+      end
+      if params[:last_name]
+        @message.last_name = params[:last_name]
+      end
+      if params[:phone]
+        @message.phone = params[:phone]
+      end
+      if params[:email]
+        @message.email = params[:email]
+      end
+      if params[:message]
+        @message.message = params[:message]
+      end
   	end
 
   	def help
@@ -16,4 +36,5 @@ class StaticPagesController < ApplicationController
 
     def landing
     end
+
 end
