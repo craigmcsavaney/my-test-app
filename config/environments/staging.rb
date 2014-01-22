2405ea9658991ff16aaa12ab56603aec8a7072e8
Craig <craigmcsavaney@gmail.com>
2014-01-22 15:09:47 -0700

    Deleted email_spec vendor plug-in, upgraded devise and devise_invitable

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -20,6 +20,9 @@ Myapplication::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+    # following added by craig, supposedly required to be false on Heroku
+  config.assets.initialize_on_precompile = false 
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
