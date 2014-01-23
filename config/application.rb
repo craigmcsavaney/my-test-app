6df77b1534ec3ae8a61fba6674d0c5bfd25b9ca0
Craig <craigmcsavaney@gmail.com>
2014-01-22 23:25:39 -0700

    Update application.rb to allow for cross-browser x-frames

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,11 @@ module Myapplication
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # following added after upgrade to 4.0 to deal with new header security requirements:
+    config.action_dispatch.default_headers = {
+        'X-Frame-Options' => 'ALLOWALL'
+    }
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
