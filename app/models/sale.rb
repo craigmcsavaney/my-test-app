a9f6e436f3637ad4c2e113fb1f35478749a529e7
Craig <craigmcsavaney@gmail.com>
2014-06-27 11:37:00 -0600

    Changes due to fact that all sales will now be associated with a purchase share (never a referral share). Extensive changes to functions that get supporter_id and simplification of case logic here. Changes in Donation.create to use Share.get_cause_id instead of Share.get_cause (deprecated).

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -29,10 +29,10 @@ class Sale < ActiveRecord::Base
 	    (100*amount) == (100*amount).to_i
   	end
 
-  	# set_buyer_share_id determines if there is a buyer associated with this sale and if so, sets the buyer_share_id to the share_id associated with the purchase share, but only if this sale is linked to a purchase path (meaning, the purchase was made by a user who had previously viewed the causebutton widget).  Note that there can be a buyer associated with a sale without there being a buyer-directed donation.
+  	# set_buyer_share_id determines if there is a buyer associated with this sale and if so, sets the buyer_share_id to the share_id associated with the purchase share, but only if this sale is associated with a serve that has been shared (meaning, the purchase was made by a user who had previously viewed the causebutton widget and shared to at least one channel).  Note that there can be a buyer associated with a sale without there being a buyer-directed donation.
   	private
 	def set_buyer_share_id
-		if self.share.channel.name.downcase == "purchase"
+		if Serve.shared?(self.serve)
 			self.buyer_share_id = self.share.id
 		end
 	end
@@ -40,34 +40,32 @@ class Sale < ActiveRecord::Base
   	# set_supporter_share_id determines if there is a supporter associated with this sale and if so, sets the supporter_share_id to the share_id associated with the supporter's share.    Note that there can be a supporter associated with a sale without there being a supporter-directed donation.
 	private
 	def set_supporter_share_id
-		# first, determine if this sale is associated with a purchase share
-		purchase_path = self.share.channel.name.downcase == "purchase"
-		# next, check to see if this sale is associated with a serve that was actually shared (meaning that the visitor posted to at least one channel before making a purchase).
+		# Note that in order for the sale to be valid, either the serve was shared or had a valid referrer, or both, so we can ignore the !shared && !referred case
+
+		# First, check to see if this sale is associated with a serve that was actually shared (meaning that the visitor posted to at least one channel before making a purchase).
 		shared = Serve.shared?(self.serve)
+
 		# next, check to see if this sale is associated with a serve that has a parent, meaning that it was created as the result of a visitor arriving at this site through link that was created from a different serve.
-		has_parent = !self.serve.referring_share_id.nil?
-		# finally, set the supporter_share_id variable to nil.  It will remain nil in the following cases:
-		# 1. When the buyer was not referred and didn't share the serve (purchase_path && !shrared && !has_parent) before making a purchase
-		# 2. When the buyer was not referred and did share the serve before making a purchase, but there is a buyer-directed donation associated with this promotion (purchase_path && shared && !has_parent && buyer_pct > 0).  For this sale, the buyer will only get credit for the buyer contribution and there will be no supporter-directed contribution.
+		referred = Serve.referred?(self.serve)
+
+		# finally, set the supporter_share_id variable to nil.  All the possible cases should be handled below, so this is just a precaution.
 		supporter_share_id = nil
 
 		case 
 
-			# first case, when the purchase was made by a visitor that had not opened the CB Widget.  In this case, the supporter is always the creator of the link that was used by the visitor to reach the site.
-			when !purchase_path
+			# first case, when the purchase was made by a referred visitor.  In this case, the supporter is always the creator of the link that was used by the visitor to reach the site.
+			when referred
+				supporter_share_id = self.serve.share.id
+
+			# following cases are for !referred && shared, as referred is handled above and !referred && !shared will not produce a sale
+
+			# second case, when the purchase was made by a non-referred visitor that had viewed the CB Widget and had shared the promotion through at least one channel, but the promotion does not offer any buyer-directed incentive.  In this case, we give the buyer credit for being a supporter that basically referred themselves to the site.
+			when self.serve.promotion.buyer_pct == 0
 				supporter_share_id = self.share.id
 
-			# second case, when the purchase was made by visitor was referred and that had viewed the CB Widget but had not shared the promotion into any of thier channels.  In this case, the supporter is the one who referred the current visitor. 
-			when purchase_path && !shared && has_parent
-				supporter_share_id = self.share.serve.share.id
-
-			# third case, when the purchase was made by a visitor that had viewed the CB Widget and had shared the promotion through at least one channel, but the promotion does not offer any buyer-directed incentive.  In this case, we give the buyer credit for being a supporter that basically referred themselves to the site, but we only give this credit if they have already shared to at least one channel.
-			when purchase_path && shared && self.serve.promotion.buyer_pct == 0
-				supporter_share_id = self.share.id
-
-			# final case, when the purchase was made by a visitor that had viewed the CB Widget and had shared the promotion through at least one channel, and for which the promotion does offer a buyer-direted incentive.  In this case, the supporter is the one who referred the current visitor (who gets the buyer credit)
-			when purchase_path && shared && has_parent && self.serve.promotion.buyer_pct > 0
-				supporter_share_id = self.share.serve.share.id
+			# final case, when the buyer was not referred and did share the serve before making a purchase, but there is a buyer-directed donation associated with this promotion.  For this sale, the buyer will only get credit for the buyer contribution and there will be no supporter-directed contribution.
+			when self.serve.promotion.buyer_pct > 0
+				supporter_share_id = nil
 
 		end
 
@@ -105,7 +103,7 @@ class Sale < ActiveRecord::Base
 			Donation.create(
 				sale_id: self.id,
 				merchant_id: self.serve.merchant.id,
-				cause_id: Share.get_cause(self.buyer).id,
+				cause_id: Share.get_cause_id(self.buyer),
 				chosen_by: "buyer",
 				amount_cents: (self.amount_cents * ((self.buyer.promotion.buyer_pct).to_f/100)).round,
 				chooser_id: buyer_id
@@ -117,7 +115,7 @@ class Sale < ActiveRecord::Base
 			Donation.create(
 				sale_id: self.id,
 				merchant_id: self.serve.merchant.id,
-				cause_id: Share.get_cause(self.supporter).id,
+				cause_id: Share.get_cause_id(self.supporter),
 				chosen_by: "supporter",
 				amount_cents: (self.amount_cents * ((self.supporter.promotion.supporter_pct).to_f/100)).round,
 				chooser_id: supporter_id
